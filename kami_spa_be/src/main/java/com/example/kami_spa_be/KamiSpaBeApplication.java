87ec8c5366b1ff314c73171bd6835a3d88d20fdb
ducvu1990 <you@example.com>
2023-04-19 16:10:13 +0700

    update

---
+++ kami_spa_be/src/main/java/com/example/kami_spa_be/KamiSpaBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kami_spa_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamiSpaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamiSpaBeApplication.class, args);
+	}
+
+}
---
+++ kami_spa_be/src/test/java/com/example/kami_spa_be/KamiSpaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kami_spa_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamiSpaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
